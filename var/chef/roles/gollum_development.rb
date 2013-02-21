56a58ff6e9ea071b211510c75197ac584b73f119
Tom Bombadil <amanibhavam@destructuring.org>
2013-02-21 00:12:34 -0800

    project_dir for tvdinners is var to avoid clutrering

--- var/chef/roles/gollum_development.rb
+++ var/chef/roles/gollum_development.rb
@@ -1,5 +1,5 @@
 name "gollum_develop"
 description "The develop overrides for a gollum unicorn"
 run_list "role[gollum]"
-override_attributes "project_dir" => Dir.pwd
+override_attributes "project_dir" => "#{Dir.pwd}/var"
 
